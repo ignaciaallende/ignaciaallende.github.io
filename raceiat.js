f719647290afa38458b4419cfd0bd6a89b2fa5cd
ignaciaallende <iallende@georgeinstitute.org.au>
2026-06-09 13:21:37 +0100

    Update raceiat.js
    update the IAT 10

--- raceiat.js
+++ raceiat.js
@@ -1,4 +1,4 @@
-define(['pipAPI','https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/IAT/iat10.js'], function(APIConstructor, iatExtension){
+define(['pipAPI', 'https://cdn.jsdelivr.net/gh/ignaciaallende/ignaciaallende.github.io@main/iat10.js'], function(APIConstructor, iatExtension){
     let API = new APIConstructor();
     let global = API.getGlobal();
 
